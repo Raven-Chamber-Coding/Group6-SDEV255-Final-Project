d40cb790cf0ff87ca5efabfe4fd4b72009cafab2
Lucas <90351520+Raven-Chamber-Coding@users.noreply.github.com>
2022-12-05 17:56:14 -0600

    Updated for javascript (Unfinished)
    Still needs to be able to view individual courses.

--- app.js
+++ app.js
@@ -8,17 +8,73 @@ const Course = require("./models/Course");
 // express app
 const app = express();
 
-// connect to mongodb (need a database)
-const dbURI = "";
+// connect to mongodb
+const dbURI = "mongodb+srv://Admin:We6778@nodetutorial.gpsyvxc.mongodb.net/nodetutorial?retryWrites=true&w=majority";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(4000))
     .catch((err) => console.log(err));
 
+// register view engine
+app.set("view engine", "ejs");
+
 //middleware & static files
 app.use(express.static("public"));
+app.use(express.static("photos"));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 // routes
+app.get("/", (req, res) => {
+    res.redirect("/homepage");
+});
+
+app.get("/homepage", (req, res) => {
+    res.render("homepage", { title: "Homepage" });
+});
 
 // course routes
+
+app.get("/courses", (req, res) => {
+    Course.find().sort({ createdAt: -1})
+        .then((result) => {
+            res.render("courses", { title: "Courses", courses: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+app.post("/courses", (req, res) => {
+    const course = new Course(req.body);
+
+    course.save()
+        .then((result) => {
+            res.redirect("/courses");
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+    Course.findById(id)
+      .then(result => {
+        res.render("details", { course: result, title: "Course Details" });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
+app.delete("/courses/:id", (req, res) => {
+    const id = req.params.id;
+    
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: "/courses" });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
