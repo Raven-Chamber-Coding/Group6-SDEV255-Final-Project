6156a36df8a6e5cdd56c02fd409a1a996eea5f39
Lucas <90351520+Raven-Chamber-Coding@users.noreply.github.com>
2022-12-16 00:31:03 +0000

    Have log in, sign up, log out working

--- controllers/controller.js
+++ controllers/controller.js
@@ -69,12 +69,16 @@ module.exports.login_get = (req, res) => {
     res.render("login", { title: "Login" });
 }
 module.exports.login_post = async (req, res) => {
-    const { email, password } = req.body; 
+    const { email, password, firstName, lastName, role } = req.body; 
+
     try {
-        const user = await User.login(email, password);
+        const user = await User.login(email, password, firstName, lastName, role);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000});
         res.status(200).json({user: user._id});
     } catch (err) {
-        res.status(400).json({})
+        handleErrors(err);
+        res.status(400).json('error, user does not exist.');
     }
 }
 // logout
