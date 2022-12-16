83f10f0b91f7e19ba6626673f7c9445db4b2d07e
Lucas <90351520+Raven-Chamber-Coding@users.noreply.github.com>
2022-12-16 20:02:03 +0000

    Minor updates

--- models/course.js
+++ models/course.js
@@ -2,6 +2,7 @@
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const User = require("../models/user")
 
 const courseSchema = new Schema({
     course: {
@@ -12,6 +13,9 @@ const courseSchema = new Schema({
         type: String,
         required: [true, 'Please add the instructor.']
     }
+//    Students: {
+//      type: String
+//    }
 }, { timestamps:true });
 
 const Course = mongoose.model("Course", courseSchema);
